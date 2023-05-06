3da3fa107359ab33c1ea71c00354f10461df42a8
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-05-06 15:53:05 +0200

    Added couple basic tests

---
+++ UnitTesting.Demo.Library.Tests/PersonTests.cs
@@ -0,0 +1,59 @@
+﻿namespace UnitTesting.Demo.Library.Tests;
+
+[TestFixture]
+public class PersonTests
+{
+    [Test]
+    public void Constructor_WhenCalled_ShouldInitializeProperties()
+    {
+        // AAA
+
+        // Arrange
+        const string firstName = "Jan";
+        const string lastName = "Kowalski";
+        const int birthYear = 2000;
+
+        // Act
+        Person person = new Person(firstName, lastName, birthYear);
+
+        // Assert
+        Assert.AreEqual(firstName, person.FirstName, "checking FirstName");
+        Assert.AreEqual(lastName, person.LastName, "checking LastName");
+        Assert.AreEqual(birthYear, person.BirthYear, "checking BirthYear");
+    }
+
+    [Test]
+    [Ignore("Replaced by the test new test")]
+    public void FullName_WhenCalled_ShouldReturnProperName()
+    {
+        // Arrange
+        const string firstName = "Jan";
+        const string lastName = "Kowalski";
+        const int birthYear = 2000;
+        const string expectedFullName = $"{firstName} {lastName}";
+        Person person = new Person(firstName, lastName, birthYear);
+
+        // Act
+        string fullName = person.FullName;
+
+        // Assert
+        Assert.AreEqual(expectedFullName, fullName);
+    }
+
+    [Test]
+    [TestCase("Jan", "Kowalski")]
+    [TestCase("Adam", "Nowak")]
+    public void FullName_WhenCalled_ShouldReturnProperName(string firstName, string lastName)
+    {
+        // Arrange
+        const int birthYear = 2000;
+        string expectedFullName = $"{firstName} {lastName}";
+        Person person = new Person(firstName, lastName, birthYear);
+
+        // Act
+        string fullName = person.FullName;
+
+        // Assert
+        Assert.AreEqual(expectedFullName, fullName);
+    }
+}
