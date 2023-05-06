0f0094bafb0836aa62ccb164ae5e9136f2183043
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-05-06 15:05:18 +0200

    Added Library project and Person class

---
+++ UnitTesting.Demo.Library/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace UnitTesting.Demo.Library;
+
+public class Person
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int BirthYear { get; }
+
+    public Person(string firstName, string lastName, int birthYear)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthYear = birthYear;
+    }
+
+    public int GetAge(int currentYear)
+    {
+        if (BirthYear > currentYear)
+        {
+            throw new ArgumentException(nameof(currentYear));
+        }
+
+        return currentYear - BirthYear;
+    }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
