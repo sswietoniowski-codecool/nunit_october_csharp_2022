d304aeaff99ca87940a864a17f4632312a523ee1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-05-06 15:24:24 +0200

    Added Application project and tested Person manually

---
+++ UnitTesting.Demo.Application/Program.cs
@@ -0,0 +1,7 @@
+﻿using UnitTesting.Demo.Library;
+
+Person person = new Person("Jan", "Kowalski", 2000);
+Console.WriteLine(person.FirstName);
+Console.WriteLine(person.LastName);
+Console.WriteLine(person.GetAge(2021));
+Console.WriteLine(person.FullName);
